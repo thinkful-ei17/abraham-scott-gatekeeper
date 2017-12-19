efdbd3b19be48331f7e15b1dcb12c385746bfc68
A. Scott Tribble <scott.tribble@As-MacBook-Pro.local>
2017-12-19 14:01:18 -0800

    pulled out user and password

--- server.js
+++ server.js
@@ -3,22 +3,13 @@
 const express = require('express');
 // you'll need to use `queryString` in your `gateKeeper` middleware function
 const queryString = require('query-string');
+const morgan = require('morgan');
 
 
 const app = express();
 
-// For this challenge, we're hard coding a list of users, because
-// we haven't learned about databases yet. Normally, you'd store
-// user data in a database, and query the database to find
-// a particular user.
-//
-// ALSO, for this challenge, we're storing user passwords as
-// plain text. This is something you should NEVER EVER EVER 
-// do in a real app. Instead, always use cryptographic
-// password hashing best practices (aka, the tried and true
-// ways to keep user passwords as secure as possible).
-// You can learn more about password hashing later
-// here: https://crackstation.net/hashing-security.htm
+app.use(morgan('common'));
+
 const USERS = [
   {
     id: 1,
@@ -69,17 +60,28 @@ const USERS = [
 //  3. looks for a user object matching the sent username and password values
 //  4. if matching user found, add the user object to the request object
 //     (aka, `req.user = matchedUser`)
+//This function should get the string value sent for the request header 'x-username-and-password'. You can use the req.get('x-username-and-password') to retrieve the header value.
+/* Use queryString(already imported at the top of server.js) to parse the values for user and pass from the request header.Specifically, use the queryString.parse method, which is designed for parsing URL query params.For instance, queryString.parse('catName=george&dogName=georgette') would produce { catName: 'george', dogName: 'georgette' }.
+// Check the user credentials against the USERS, array.You'll probably want to use the .find array method for this.
+// If a matching user is found, set req.user to that user.Otherwise, req.user should be either null or undefined.Note that you should never explicitly set a value to undefined, but depending on your implementation, req.user might end up being undefined.
+// The easiest way to test your app as you work on it is to use Postman.Make a GET request to base - url -for-your - app / api / users / me.You'll need to set a request header in the headers tab, with a key x-username-and-password and a value like user=joeschmoe@business.com&pass=password. */
+
 function gateKeeper(req, res, next) {
-  // your code should replace the line below
+  console.log('gateKeeper ran');
+  const userString = (req.headers['x-username-and-password']); 
+  console.log(queryString.parse(userString)); 
+  const { user, pass } = queryString.parse(userString);
+  console.log(user, pass);
   next();
 }
 
+app.use(gateKeeper);
 // Add the middleware to your app!
 
 // this endpoint returns a json object representing the user making the request,
 // IF they supply valid user credentials. This endpoint assumes that `gateKeeper` 
 // adds the user object to the request if valid credentials were supplied.
-app.get("/api/users/me", (req, res) => {
+app.get('/api/users/me', (req, res) => {
   // send an error message if no or wrong credentials sent
   if (req.user === undefined) {
     return res.status(403).json({ message: 'Must supply valid user credentials' });
@@ -91,6 +93,9 @@ app.get("/api/users/me", (req, res) => {
   return res.json({ firstName, lastName, id, userName, position });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Your app is listening on port ${process.env.PORT}`);
+// });
+
+app.listen(process.env.PORT || 8080, () => console.log(
+  `Your app is listening on port ${process.env.PORT || 8080}`));
